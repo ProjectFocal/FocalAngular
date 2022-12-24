42aa21609bdbc98e6ff45e986d9b7b2ea70ebc45
trulyNathan2 <nathanaruna07@gmail.com>
2022-12-23 20:54:30 -0500

    stars fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import 'css-doodle';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +14,8 @@ export class LoginComponent implements OnInit {
    }
 
   ngOnInit(): void {
+
+
   }
 
 }
